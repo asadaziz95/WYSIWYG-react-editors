4ce98f368e47327cda67e59248ea6879e887ce46
asadaziz <asadazizjs@gmail.com>
2021-01-25 17:29:55 +0500

    integerated text editors

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// import {Editor, EditorState} from 'draft-js';
+// import '../node_modules/draft-js/dist/Draft.css';
+import { Editor } from 'react-draft-wysiwyg';
+import '../node_modules/react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+//import logo from './logo.svg';
+// import {Editor, EditorState} from 'draft-js';
+// import 'draft-js/dist/Draft.css';
+// import { Editor } from 'react-draft-wysiwyg';
+// import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
+ import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function MyEditor() {
+//   const [editorState, setEditorState] = React.useState(
+//     () => EditorState.createEmpty(),
+//   );
+
+//   return <Editor editorState={editorState} onChange={setEditorState} />;
+// }
+
+
+
+
+const EditorComponent = () => <Editor />
+const  App=()=> {
+  // const [editorState, setEditorState] = React.useState(
+  //   () => EditorState.createEmpty(),
+  // );
+
+  // return <Editor editorState={editorState} onChange={setEditorState} />;
+  // const [editorState, setEditorState] = React.useState(
+  //   () => EditorState.createEmpty(),
+  // );
+
+  // return <Editor editorState={editorState} onChange={setEditorState} />;
+  // return (
+  //   <div className="App">
+  //     <MyEditor />
+  //   </div>
+  // );
+  return <EditorComponent />
 }
 
 export default App;
